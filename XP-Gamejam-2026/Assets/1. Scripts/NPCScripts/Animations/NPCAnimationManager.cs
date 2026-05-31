afc67f1e023f156a464b95161454a9282b779124
Danny-vD <dvduist@hotmail.nl>
2026-05-31 05:45:21 +0200

    Add `NPCAnimationManager`

---
+++ XP-Gamejam-2026/Assets/1. Scripts/NPCScripts/Animations/NPCAnimationManager.cs
@@ -0,0 +1,32 @@
+﻿using EditorAttributes;
+using UnityEngine;
+using VDFramework;
+using XPGJ2026.PlayerCollisionHandlers.Interfaces;
+
+namespace XPGJ2026.NPCScripts.Animations
+{
+	public class NPCAnimationManager : BetterMonoBehaviour, IPlayerCollisionHandler
+	{
+		[SerializeField, Required(fixMode: ReferenceFixMode.Self)]
+		private Animator animator;
+
+		[Header("Parameters")]
+		[SerializeField, AnimatorParamDropdown(nameof(animator))]
+		private int hitTriggerID;
+		
+		private void Reset()
+		{
+			animator = GetComponent<Animator>();
+		}
+
+		public void PlayHitAnimation()
+		{
+			animator.SetTrigger(hitTriggerID);
+		}
+
+		public void CollidedWithPlayer()
+		{
+			PlayHitAnimation();
+		}
+	}
+}
