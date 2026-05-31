64f0a67046780b8e5a038ccddfdd6848e8df7c24
Danny-vD <dvduist@hotmail.nl>
2026-05-31 12:56:18 +0200

    Add main menu and victory scene

---
+++ XP-Gamejam-2026/Assets/1. Scripts/Ending/VictoryTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using VDFramework;
+
+namespace XPGJ2026.Ending
+{
+	public class VictoryTrigger : BetterMonoBehaviour
+	{
+		[SerializeField]
+		private int victorySceneIndex;
+		
+		private void OnTriggerEnter(Collider other)
+		{
+			SceneManager.LoadScene(victorySceneIndex);
+		}
+	}
+}
